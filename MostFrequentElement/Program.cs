8ebf3ceaa46152c04b965cd2b22bc05d1220015e
sagarchopranimbus <sagar.chopra@nimbusdigital.com>
2023-09-16 11:56:22 +0530

    Most frequent element in array

---
+++ MostFrequentElement/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace MostFrequentElement
+{
+    internal class Program
+    {
+        // Driver code
+        static void Main()
+        {
+            int[] arr = new int[] { 40, 50, 30, 40, 50, 30, 30, 50, 50, 50 };
+            int n = arr.Length;
+
+            Console.Write(mostFrequent2(arr, n));
+        }
+
+        private static int mostFrequent1(int[] arr, int n)
+        {
+            Dictionary<int,int> set = new Dictionary<int,int>();
+
+            for(int i = 0; i<n; i++)
+            {
+                int key = arr[i];
+                if (set.ContainsKey(key))
+                {
+                    int freq = set[key];
+                    freq++;
+                    set[key] = freq;
+                }
+                else
+                {
+                    set.Add(key, 1);
+                }
+            }
+
+            int maxCount = 0;
+            int result = -1;
+
+            foreach(KeyValuePair<int,int> kvp in set)
+            {
+                if(kvp.Value > maxCount)
+                {
+                    maxCount = kvp.Value;
+                    result = kvp.Key;
+                }
+            }
+
+            return result;
+        }
+
+        private static int mostFrequent2(int[] arr, int n)
+        {
+            int maxValue = arr.Max();
+
+            int[] hashArray = new int[maxValue+1];
+
+            for (int i = 0; i < n; i++)
+            {
+                hashArray[arr[i]]++;
+            }
+
+            int maxCount = 0;
+            int result = -1;
+
+            for(int i = 0; i< hashArray.Length; i++)
+            {
+                if (hashArray[i] > maxCount)
+                {
+                    maxCount = hashArray[i];
+                    result = i;
+                }
+            }           
+
+            return result;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Practice
+﻿using System;
+
+class BinarySearch
 {
-    internal class Program
+    static int Search(int[] arr, int target)
     {
-        static void Main(string[] args)
+        int left, mid, right;
+
+        left = 0;
+        right = arr.Length - 1;
+        
+
+        while(left <= right)
         {
-            Console.WriteLine("Hello, World!");
+            mid = left + (right - left) / 2;
+
+            if (target == arr[mid])
+                return mid;
+            else if(target > arr[mid])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
         }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] sortedArray = { 2, 5, 8, 12, 16, 23, 38, 45, 56, 72 };
+        int target = 23;
+
+        int index = Search(sortedArray, target);
+
+        if (index != -1)
+            Console.WriteLine($"Element {target} found at index {index}.");
+        else
+            Console.WriteLine($"Element {target} was not found in the array.");
     }
 }
